bc92dc38c4975942019690a578068fafd43fe9b3
Jeff Rose <rosejn@gmail.com>
2010-10-01 00:19:30 +0200

    arranging for new namespaces

--- test/overtone_contrib/test/core.clj
+++
@@ -1,6 +0,0 @@
-(ns overtone-contrib.test.core
-  (:use [overtone-contrib.core] :reload)
-  (:use [clojure.test]))
-
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
