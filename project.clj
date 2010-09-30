e4d023774f98c58fbdb727ca7c4148b9e1563cad
Jeff Rose <rosejn@gmail.com>
2010-10-01 00:14:59 +0200

    filling out project info

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
-(defproject overtone-contrib "1.0.0-SNAPSHOT"
-  :description "FIXME: write"
+(defproject overtone-contrib "0.1.3-SNAPSHOT"
+  :description "Hardware device support and other experimental odds and ends related to project Overtone."
+  :url "http://project-overtone.org"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]])
